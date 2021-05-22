c70f543fa5a16054f9a449bef17627878b36012c
Bence Nagy <bence@underyx.me>
2021-05-22 15:34:09 +0300

    Make package not stub-only

---
+++ src/hypeshed/__init__.py
@@ -0,0 +1,6 @@
+"""
+Seems like stub-only packages are supported only to supplement untyped libraries
+https://www.python.org/dev/peps/pep-0561/#stub-only-packages
+
+This file just makes it so this isn't a stub-only package.
+"""
